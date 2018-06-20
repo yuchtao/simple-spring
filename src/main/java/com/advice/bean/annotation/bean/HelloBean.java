1d8bdcb9c584fb8051f8c169e3d88eca958d38db
yuch <keyuqi@taidizh.com>
2018-06-20 17:44:01 +0800

    删掉无用代码

--- src/main/java/com/advice/bean/annotation/bean/HelloBean.java
+++
@@ -1,8 +0,0 @@
-package com.advice.bean.annotation.bean;
-
-/**
- * Created by yuch on 2017/8/31.
- */
-public interface HelloBean {
-    void helloWorld();
-}
--- src/main/java/com/advice/bean/annotation/bean/YuchService.java
+++
@@ -1,8 +0,0 @@
-package com.advice.bean.annotation.bean;
-
-/**
- * Created by yuch on 2018/6/14.
- */
-public interface YuchService {
-    void output(String text);
-}
